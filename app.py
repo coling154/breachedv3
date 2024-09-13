3463adb9805064a9b4e595dccd6cafc3273e0241
Colin Gasiewicz <gasiewca@clarkson.edu>
2024-09-13 14:18:03 -0400

    start flask part

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/email')
+def email():
+    return ''
+
+
+@app.route('/password')
+def password():
+    return ''
+
+
 if __name__ == '__main__':
     app.run()
